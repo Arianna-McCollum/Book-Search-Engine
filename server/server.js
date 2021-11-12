217ef082ee6a4e982b32f7edf2b594a9e3128ab1
Electrolion <AMccollum71@gmail.com>
2021-11-12 12:25:51 -0500

    Update server.js apollo & auth.js working with graphql

--- server/server.js
+++ server/server.js
@@ -10,26 +10,19 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const startServer = async () => {
-  // create a new Apollo server and pass in schema data
+// create a new Apollo server and pass in schema data
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers,
     context: authMiddleware
   });
 
-  // Start the Apollo server
-  await server.start();
-
   // integrate Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 
   // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
 
-// Initialize the Apollo server
-startServer();
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
@@ -48,4 +41,5 @@ if (process.env.NODE_ENV === 'production') {
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
